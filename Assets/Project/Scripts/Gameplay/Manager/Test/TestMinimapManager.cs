3a89db6debfde02951fac36543f9fed3469a63c6
Sketh <www.sketh007@gmail.com>
2025-08-04 16:49:23 +0530

    Capture Texture from Overhead and Player
    - Project
        [=] Changing Minimap_RT Color Format | Width/Height
        [=] Adding Environment layer
    - Scene
        [=] Test
            {+} Adding test trees
            {+} Adding Environment to LayerMask | Trees are static for now
    - Scripts
        [=] TestMinimapManager
            {+} Keeping a private texture for storing overhead texture
            {+} Save Image from Texture logic
            {+} Using same camera to get overhead view of the whole map | Changing settings only

--- Assets/Project/Scripts/Gameplay/Manager/Test/TestMinimapManager.cs
+++ Assets/Project/Scripts/Gameplay/Manager/Test/TestMinimapManager.cs
@@ -1,16 +1,54 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+
+using UnityEngine.Experimental.Rendering;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CurseOfNaga.Gameplay.Managers.Test
 {
     public class TestMinimapManager : MonoBehaviour
     {
-        [SerializeField] private Camera _mainCamTerrain, _mainCamOther;
+        [SerializeField] private Camera _mainCamTerrain;
         [SerializeField] private RenderTexture _minimapRT;
         [SerializeField] private RawImage _minimapImg;
+        [SerializeField] private Transform _player;
+        private const float yPos = 30f;
 
+        [Header("Map-Overhead Camera")]
+        [SerializeField] private LayerMask _bgMask;
+        [SerializeField] private int _bgRefreshRate = 5;
+        // [SerializeField] private 
+        private float _bgProjectionSize = 50f;
+        private bool _takeOverhead;
+
+
+        [Header("Player Location Camera")]
+        [SerializeField] private LayerMask _playerLocationCullingGroup;
+        private float _playerProjectionSize = 15f;
+
+        private Texture2D _bgMapTex;
         private RenderTexture _currentRT;
         public bool updateRender = false;
+
+        private CancellationTokenSource _cts;
+
+        private void OnDestroy()
+        {
+            _cts.Cancel();
+        }
+
+        private void Start()
+        {
+            _cts = new CancellationTokenSource();
+
+            _bgMapTex = new Texture2D(_minimapRT.width, _minimapRT.height, _minimapRT.graphicsFormat, TextureCreationFlags.None);
+            //_bgMapTex = new Texture2D(_minimapRT.width, _minimapRT.height, gfxFormat, false, true);
+
+            _takeOverhead = true;
+            UpdateBGTexture();
+        }
 
         private void Update()
         {
@@ -26,18 +64,64 @@ namespace CurseOfNaga.Gameplay.Managers.Test
             //Get the Texture from _mainCamOther    -> Background transperant for this one
             //Get the Texture from _mainCamTerrain
             //Overlap _mainCamOther to Terrain
+
+            Vector3 finalPos = Vector3.zero;
             _currentRT = RenderTexture.active;
             RenderTexture.active = _minimapRT;
 
+            if (_takeOverhead)
+            {
+                _takeOverhead = false;
+
+                _mainCamTerrain.cullingMask = _bgMask;
+                _mainCamTerrain.orthographicSize = _bgProjectionSize;
+
+                finalPos.y = yPos;
+                _mainCamTerrain.transform.position = finalPos;
+
+                _mainCamTerrain.Render();
+
+                var gfxFormat = TextureFormat.ARGB32;
+                _bgMapTex.ReadPixels(new Rect(0, 0, _minimapRT.width, _minimapRT.height), 0, 0);
+                _bgMapTex.Apply();
+                // SaveImage(image);
+
+                //Reset back to player settings
+                _mainCamTerrain.cullingMask = _playerLocationCullingGroup;
+                _mainCamTerrain.orthographicSize = _playerProjectionSize;
+            }
+
+            finalPos = _player.position;
+            finalPos.y = yPos;
+            _mainCamTerrain.transform.position = finalPos;
+
             _mainCamTerrain.Render();
+            _bgMapTex.ReadPixels(new Rect(0, 0, _minimapRT.width, _minimapRT.height), 0, 0);
+            _bgMapTex.Apply();
 
-            Texture2D image = new Texture2D(_minimapRT.width, _minimapRT.height);
-            image.ReadPixels(new Rect(0, 0, _minimapRT.width, _minimapRT.height), 0, 0);
-            image.Apply();
-
-            _minimapImg.texture = image;
-
+            _minimapImg.texture = _bgMapTex;
             RenderTexture.active = _currentRT;
+        }
+
+        private async void UpdateBGTexture()
+        {
+            await Task.Delay(_bgRefreshRate * 1000);
+            if (_cts.IsCancellationRequested) return;
+
+            _takeOverhead = true;
+            UpdateBGTexture();
+        }
+
+        private void SaveImage(Texture2D textureToSave)
+        {
+            byte[] imgData = textureToSave.EncodeToPNG();
+            string timeNow = $"{System.DateTime.Now.Hour}_{System.DateTime.Now.Minute}_{System.DateTime.Now.Second}";
+            string filePath = Path.Combine(Application.persistentDataPath, $"Img_{timeNow}.png");
+
+            FileStream saveStream = File.Create(filePath);
+            saveStream.Write(imgData);
+            saveStream.Close();
+            Debug.Log($"Saved File to: {filePath}");
         }
     }
 }
