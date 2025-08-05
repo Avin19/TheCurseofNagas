729d9eaf348f21711c33aeafd6c480ccdf792ad9
Sketh <www.sketh007@gmail.com>
2025-08-05 10:36:39 +0530

    Minimap | Offset Player 1
    - Scripts
        [=] TestMinimapManager
            {+} Minimap settings for Player
            {+} Displaying object position on the minimap
                <~> Offsetting player pos according to the size of the minimapRT

--- Assets/Project/Scripts/Gameplay/Manager/Test/TestMinimapManager.cs
+++ Assets/Project/Scripts/Gameplay/Manager/Test/TestMinimapManager.cs
@@ -16,6 +16,13 @@ namespace CurseOfNaga.Gameplay.Managers.Test
         [SerializeField] private Transform _player;
         private const float yPos = 30f;
 
+        [Header("Minimap Settings")]
+        [SerializeField, Range(0.1f, 5f)] private float _minimapRefreshRate = 1;
+        // [SerializeField] private float _scaleFactor = 5;
+        [SerializeField] private int _mapDimension = 10, _minimapDimension = 512, _markerDimension = 5;
+        [SerializeField] private Color _playerColor;
+        [SerializeField] private Transform _obj1;
+
         [Header("Map-Overhead Camera")]
         [SerializeField] private LayerMask _bgMask;
         [SerializeField] private int _bgRefreshRate = 5;
@@ -47,6 +54,7 @@ namespace CurseOfNaga.Gameplay.Managers.Test
             //_bgMapTex = new Texture2D(_minimapRT.width, _minimapRT.height, gfxFormat, false, true);
 
             _takeOverhead = true;
+            UpdateMinimap();
             UpdateBGTexture();
         }
 
@@ -83,7 +91,7 @@ namespace CurseOfNaga.Gameplay.Managers.Test
 
                 var gfxFormat = TextureFormat.ARGB32;
                 _bgMapTex.ReadPixels(new Rect(0, 0, _minimapRT.width, _minimapRT.height), 0, 0);
-                _bgMapTex.Apply();
+                // _bgMapTex.Apply();
                 // SaveImage(image);
 
                 //Reset back to player settings
@@ -95,21 +103,59 @@ namespace CurseOfNaga.Gameplay.Managers.Test
             finalPos.y = yPos;
             _mainCamTerrain.transform.position = finalPos;
 
-            _mainCamTerrain.Render();
-            _bgMapTex.ReadPixels(new Rect(0, 0, _minimapRT.width, _minimapRT.height), 0, 0);
+            //Copy an area from the saved bg texture
+            // _mainCamTerrain.Render();
+            // _bgMapTex.ReadPixels(new Rect(0, 0, _minimapRT.width, _minimapRT.height), 0, 0);
+
+            int xOffset = _minimapDimension / 2, yOffset = _minimapDimension / 2;
+            int scaleFactor = _minimapDimension / (_mapDimension * _mapDimension);
+            // int startX = (int)(_obj1.position.x * _scaleFactor) + xOffset
+            //     , startY = (int)(_obj1.position.z * _scaleFactor) + yOffset;
+            int startX = (int)(_player.position.x * scaleFactor) + xOffset
+                , startY = (int)(_player.position.z * scaleFactor) + yOffset;
+
+            startX -= _markerDimension / 2;
+            startY -= _markerDimension / 2;
+
+            if (_markerDimension % 2 == 1)
+            {
+                startX -= 1; startY -= 1;
+            }
+
+            // Debug.Log($"startX: {startX} | startY: {startY}");
+            for (int i = 0; i < _markerDimension; i++)
+            {
+                for (int j = 0; j < _markerDimension; j++)
+                {
+                    _bgMapTex.SetPixel(startX + i, startY + j, _playerColor);
+                }
+            }
             _bgMapTex.Apply();
 
             _minimapImg.texture = _bgMapTex;
             RenderTexture.active = _currentRT;
         }
 
+        private async void UpdateMinimap()
+        {
+            while (true)
+            {
+                await Task.Delay((int)(_minimapRefreshRate * 1000));
+                if (_cts.IsCancellationRequested) return;
+
+                RenderCameraHelper();
+            }
+        }
+
         private async void UpdateBGTexture()
         {
-            await Task.Delay(_bgRefreshRate * 1000);
-            if (_cts.IsCancellationRequested) return;
+            while (true)
+            {
+                await Task.Delay(_bgRefreshRate * 1000);
+                if (_cts.IsCancellationRequested) return;
 
-            _takeOverhead = true;
-            UpdateBGTexture();
+                _takeOverhead = true;
+            }
         }
 
         private void SaveImage(Texture2D textureToSave)
