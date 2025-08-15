5558a517385a9776fd39147b980e954d2796ab90
Sketh <www.sketh007@gmail.com>
2025-08-15 11:37:07 +0530

    DialogueManager | Basic JSON loading

---
+++ Assets/Project/Scripts/DialogueSystem/Test/DialogueManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+using CurseOfNaga.DialogueSystem.Runtime;
+using CurseOfNaga.Utils;
+
+namespace CurseOfNaga.DialogueSystem.Test
+{
+    //TODO: This will be a non-monobehaviour script
+    public class DialogueManager : MonoBehaviour
+    {
+        /*
+        * - This will contain all the dialogue information/data to be used in the game
+        * - Provide dialogue to display on the screen
+        * - This will track all the dialogue status/progress
+        *   [=] Ongoing Conversation
+        *       {+} Which dialogue index is the NPC / Player at while interacting
+        *   [=] Which dialogue options should be available to the Player for interaction
+        *   [=] Evaluate conditions to go to the appropriate next dialogue
+        * - Recieve NPC decision to talk or not?
+        *   [=] Player does not fulfill enough points/requirements to talk to the NPC
+        * - 
+        */
+
+        private DialogueTemplate _dialogueTemplate;
+        private int _currDialogueIndex = 0;
+
+        private const string _FILENAME = "Dialogues_SerializeTest.json";
+
+        public async void LoadDialoguesJson()
+        {
+            // fileName += UniversalConstant.JSON_EXTENSION;
+            string pathToJson = System.IO.Path.Join(Application.streamingAssetsPath, _FILENAME);
+            Debug.Log($"Loadiing Json from: {pathToJson}");
+
+            if (!System.IO.File.Exists(pathToJson))
+            {
+                Debug.LogError($"Invalid Dialogues JSON file path: {pathToJson}");
+                return;
+            }
+
+            FileDataHelper fileDataHelper = new FileDataHelper();
+            string jsonData = await fileDataHelper.GetFileData_Async(pathToJson);
+
+            _dialogueTemplate = JsonUtility.FromJson<DialogueTemplate>(jsonData);
+            // Debug.Log($"Dialogue Template: \n{dialogueTemplate} | jsonData: {jsonData}"); return;         //TEST
+        }
+
+        //Track progress of ongoing conversation
+    }
+}
