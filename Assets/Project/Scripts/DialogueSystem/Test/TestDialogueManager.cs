2797db33aab7b8c5680e963ce0f1e0485ce7d6ba
Sketh <www.sketh007@gmail.com>
2025-08-18 11:39:14 +0530

    Sialogue Interaction | Evaluate Dialogue 3 | Choices 1
    - TestDialogueManager
        [=] Check for choices and invoke action to display them
    - TestDialogueSystemCanvas
        [=] Show multiple choices logic
    - Dialogues_SerializesTest.json
        [=] Adding Player test dialogues
        [=] Adding dialogues to Choice Nodes also

--- Assets/Project/Scripts/DialogueSystem/Test/TestDialogueManager.cs
+++ Assets/Project/Scripts/DialogueSystem/Test/TestDialogueManager.cs
@@ -76,7 +76,8 @@ namespace CurseOfNaga.DialogueSystem.Test
         // Track progress of ongoing conversation
         public void EvaluateAndLoadDialogue(InteractionType type, int uid, int npcID)
         {
-            if (type != InteractionType.INTERACTING_WITH_NPC) return;
+            if (type != InteractionType.INTERACTING_WITH_NPC || npcID == -1 || uid == -1) return;
+
 
             /*
             * - What can be there to evaluate for a dialogue before presenting
@@ -93,8 +94,16 @@ namespace CurseOfNaga.DialogueSystem.Test
             */
 
             DialogueData dialogueData = _dialogueTemplate.characters[npcID].dialogues_list[_npcDialogueTracker[uid]];
+            string tempString;
+            int nextDIndex;
 
             //TODO: Configure this in the future if needed
+
+            //Get the dialogue
+            tempString = dialogueData.dialogue;
+            TestDialogueMainManager.Instance.OnShowDialogue?.Invoke(tempString);
+            Debug.Log($"Shwoing Dialogue | tempString: {tempString} | Ports: {dialogueData.ports.Count}");
+
             //Check dialogue type
             int dialogueType = dialogueData.type;
             switch (dialogueType)
@@ -110,8 +119,22 @@ namespace CurseOfNaga.DialogueSystem.Test
 
                 //We would have to iterate over every choice and check if the requirements are met or not
                 case (int)DialogueType.CHOICE:
+                    TestDialogueMainManager.Instance.OnPlayerInteraction?
+                        .Invoke(InteractionType.INTERACTING_WITH_NPC, UNSET_VAL, -(int)DialogueType.CHOICE);
 
-                    break;
+                    int choicesCount = dialogueData.ports.Count;
+
+                    //Iterate over each choice and display buttons
+                    for (int i = 0; i < choicesCount; i++)
+                    {
+                        tempString = dialogueData.ports[i].target_uid;
+                        int.TryParse(tempString.Substring(0, 3), out npcID);
+                        int.TryParse(tempString.Substring(6, 3), out nextDIndex);
+                        TestDialogueMainManager.Instance.OnShowDialogue
+                            ?.Invoke(_dialogueTemplate.characters[npcID].dialogues_list[nextDIndex].dialogue);
+                    }
+
+                    return;
 
                 case 420:
                     //Evaluate the conditions to show correct dialogue
@@ -126,19 +149,37 @@ namespace CurseOfNaga.DialogueSystem.Test
                     break;
             }
 
-            //Get the dialogue
-            string tempString = dialogueData.dialogue;
-            TestDialogueMainManager.Instance.OnShowDialogue?.Invoke(tempString);
 
             //Get the Next Node ID
-            if (dialogueData.ports == null)     //Reached End Of Conversation
+            //FIXME: JsonUtility does not returns null | Newtonsoft.Json will return null
+            // if (dialogueData.ports == null)     //Reached End Of Conversation
+            if (dialogueData.ports.Count == 0)     //Reached End Of Conversation
             {
+                /* What happens when the conversation reaches EOC [End Of Conversation]
+                * - The current dialogueIndex is at the end of a Node
+                *   [=] Need to place it in such a place where the next conversation takes place
+                *       {+} Normally, the index should be placed to point to the next part of the story
+                *       {+} What if, the player has began a quest? In this case:
+                *           <~> The player might want to interact again with the NPC to hear about the 
+                *               quest one more time, if the player wasnt paying attention (assuming always).
+                *           <~> This would require revisiting the dialogue-tree from the start.
+                *           <~> There wont be any choices as the NPC has given the Quest and all objectives will be set.
+                *           <~> The dialogueIndex should start at the base of that particular Quest's DialogueGraph
+                *               [IMP] Need to update once the player has made progress
+                *       {+} What if, the player is casually talking to an NPC? In this case:
+                *           <~> The NPC has only 1 choice available at this point of story
+                *               [;] The NPC should be able to repeat the same set of dialogue for its normal routine
+                *           <~> The NPC has multiple choices to select from
+                *               [;] Same as above, the player just selects a choice but the flow will be the same
+                *           <~> The dialogueIndex should land at the position where it started from.
+                *               [IMP] Need to update index once the player has made progress
+                */
+
                 TestDialogueMainManager.Instance.OnPlayerInteraction?
-                    .Invoke(InteractionType.INTERACTING_WITH_NPC, DEFAULT_VAL, UNSET_VAL);
+                    .Invoke(InteractionType.INTERACTING_WITH_NPC, UNSET_VAL, UNSET_VAL);
                 return;
             }
 
-            int nextDIndex;
             tempString = dialogueData.ports[0].target_uid;
             int.TryParse(tempString.Substring(6, 3), out nextDIndex);
 
--- Assets/Project/Scripts/DialogueSystem/Test/TestDialogueSystemCanvas.cs
+++ Assets/Project/Scripts/DialogueSystem/Test/TestDialogueSystemCanvas.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 using static CurseOfNaga.Global.UniversalConstant;
 
@@ -8,6 +9,11 @@ namespace CurseOfNaga.DialogueSystem.Test
     {
         [SerializeField] private GameObject _dialogueRect;
         [SerializeField] private TMPro.TMP_Text _dialogueTxt;
+        [SerializeField] private Button[] _dialogueChoiceBts;
+        [SerializeField] private TMPro.TMP_Text[] _dialogueChoicesTxt;
+
+        private bool _showChoice;
+        private int _currDialogueIndex;
 
         private void OnDisable()
         {
@@ -24,15 +30,47 @@ namespace CurseOfNaga.DialogueSystem.Test
         {
             TestDialogueMainManager.Instance.OnPlayerInteraction += UpdateUIForInteraction;
             TestDialogueMainManager.Instance.OnShowDialogue += UpdateDialogueText;
+
+            for (int i = 0; i < _dialogueChoiceBts.Length; i++)
+            {
+                int tempIndex = i;
+                _dialogueChoiceBts[i].onClick.AddListener(() => ChoseDialogue(tempIndex));
+            }
         }
 
-        private void UpdateUIForInteraction(InteractionType interactionType, int value = 1, int otherVal = -1)
+        private void ChoseDialogue(int btIndex)
         {
-            Debug.Log($"UpdateUIForInteraction | value: {value} | otherVal: {otherVal}");
+            Debug.Log($"Player chose dialogue. Index: {btIndex}");
+        }
+
+        private void UpdateUIForInteraction(InteractionType interactionType, int value = -1, int otherVal = -1)
+        {
+            Debug.Log($"UpdateUIForInteraction | interactionType: {interactionType} | value: {value} | otherVal: {otherVal}");
             switch (interactionType)
             {
                 case InteractionType.INTERACTING_WITH_NPC:
-                    if (otherVal != -1)
+
+                    if (otherVal == -(int)DialogueType.CHOICE)
+                    {
+                        _showChoice = true;
+                        // _dialogueTxt.gameObject.SetActive(false);
+
+                        for (int i = 0; i < _dialogueChoiceBts.Length; i++)
+                            _dialogueChoiceBts[i].gameObject.SetActive(true);
+
+                        break;
+                    }
+                    else if (_currDialogueIndex > 0)            //Choices have been shown
+                    {
+                        _currDialogueIndex = 0;
+                        _showChoice = false;
+                        // _dialogueTxt.gameObject.SetActive(true);
+
+                        for (int i = 0; i < _dialogueChoiceBts.Length; i++)
+                            _dialogueChoiceBts[i].gameObject.SetActive(false);
+                    }
+
+                    if (value != -1)
                         _dialogueRect.SetActive(true);
                     else
                         _dialogueRect.SetActive(false);
@@ -43,7 +81,10 @@ namespace CurseOfNaga.DialogueSystem.Test
 
         private void UpdateDialogueText(string dialogue)
         {
-            _dialogueTxt.text = dialogue;
+            if (_showChoice)
+                _dialogueChoicesTxt[_currDialogueIndex].text = dialogue;
+            else
+                _dialogueTxt.text = dialogue;
         }
     }
 }
